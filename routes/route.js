0e98257f427ddda17b1de8903cda8a6f0d1b7951
Kamyar Lajani <kamolajani1@gmail.com>
2019-07-15 16:41:54 +0300

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,57 +1,92 @@
 const express = require("express");
 const route = express();
-const bodyParser = require("body-parser");
+const https = require('https');
 const path = require("path");
+const bodyParser = require("body-parser");
 route.use(bodyParser.json());
-route.use(bodyParser.urlencoded({extended: false}));
-route.set("view engine", "pug");
-route.set("views", path.join(__dirname , "../views"));
-route.use(['/public', '/page/public'], express.static('public'));
-const mongo = require("mongodb").MongoClient;
-let mongoUrl = "mongodb://127.0.0.1:27017/";
+const mongoose = require('mongoose');
+const formidable = require('formidable');
+route.set('view engine', 'pug');
+route.set('views', path.join(__dirname , "/views"));
+let users = require('./model.js');
+let fs = require('fs');
+let articles = require(path.join(__dirname , "/model.js"));
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
-// limit the documents to 5 for each page
-let skip, currentPage, totalPages, counter, limit = 5;
 
-// redirecting user from '/' to 'page/1'
-route.get('/', (req, res)=>{
-    res.redirect('/page/1');
-});
+(async ()=> {
+    var sllaw;
+    const browser = await puppeteer.launch({headless: true});
+    let page  = await browser.newPage();
+    await page.goto('https://instagram.com/accounts/login');
+    await page.waitForSelector('input[name="username"]');
+    await page.waitForSelector('input[name="password"]');
+    await page.waitForSelector('button[type="submit"]')
+    await page.click('input[name="username"]');
+    await page.type('input[name="username"]','router.js');
+    await page.click('input[name="password"]');
+    await page.type('input[name="password"]','bestprogrammer123');
+    await page.click('button[type="submit"]');
+    await page.waitFor(7000);
 
-route.get('/page/:num', (req, res)=>{
-    // number of page by parameter in URL
-    currentPage = parseInt(req.params.num);
-    // skip: where should index of documents start at
-    skip = (limit*currentPage) - limit
+    // one hour
+    let hour = setInterval(perHour, 3600000);
+    let hashtags = ["javascript", "html", "coding"];
    
-    mongo.connect(mongoUrl, {useNewUrlParser: true}, (err, database)=>{
-        if(err) throw err;
-        let db = database.db("test");
-        
-        db.collection("users").countDocuments((err, result)=>{
-            counter = result;
-           
+    let hashtagIndex = 0;
+    async function perHour(){
+        if(hashtagIndex >= hashtags.length) {
+            hashtagIndex = 0;
+        }
+       
+        await page.goto(`https://www.instagram.com/explore/tags/${hashtags[hashtagIndex]}/`);
+        await page.evaluate(()=>{
+            let func = setInterval(loops, 15000);
+            let i=0, likes=0;
+                function loops() {
+                    // stop the loop
+                    if(likes <= 25){
+                        
+                        document.querySelectorAll('div[class="KL4Bh"]')[i+9].click();
+                        setTimeout(() => {
+                            
+                                
+                                let like = document.querySelector('button[class="dCJp8 afkep _0mzm-"] span');  
+                        
+                                if(like.getAttribute("aria-label") == "Like") {
+                                    document.querySelector('button[class="dCJp8 afkep _0mzm-"]').click();
+                                    
+                                    setTimeout(() => {
+                                        document.querySelector('button[class="ckWGn"]').click();
+                                    }, 5000);
+                                    likes++;
+                                }
+                                else {
+                                    document.querySelector('button[class="ckWGn"]').click();
+                                    
+                                }
+                                i++;
+                            
+                        }, 5000);
+                    }
                
+                    else {
+                        clearInterval(func);
+                        
+                    }
+                      
+                    
+                }
+                loops();
+            
         });
 
-        setTimeout(()=>{
-            totalPages = Math.ceil(counter / limit);
-           
-            db.collection("users").find({}).limit(limit).skip(skip).toArray((err, result)=>{
+        hashtagIndex++;
+    }
+    perHour();
             
-                res.render("index.pug", {result, totalPages, currentPage, limit});
-            });
-           
-            database.close();
-        }, 15);
-    });
-
-
-    
-});
-
-
-
+})();
 
 
 module.exports = route;
